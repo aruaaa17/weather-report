ee416fc36d879a46cc3300d14e09ada358ffbb5f
April Zhang <aprilzh23@gmail.com>
2023-06-07 16:43:25 -0700

    Add event listener to change landscape

--- src/index.js
+++ src/index.js
@@ -22,19 +22,30 @@ const decreaseTemp = () => {
 // Change the color of the temperature text
 const tempValue = document.querySelector("#tempValue");
 
+// Change the landscape based on the temperature
+const landscape = document.querySelector("#landscape");
+
 const changeTempColor = () => {
     tempValue.classList.remove("red", "orange", "yellow", "green", "light-blue");
     
     if (state.temp >= 80) {
         tempValue.classList.add("red");
+        landscape.innerHTML = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
     } else if (state.temp >= 70) {
         tempValue.classList.add("orange");
+        landscape.innerHTML = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+
     } else if (state.temp >= 60) {
         tempValue.classList.add("yellow");
+        landscape.innerHTML = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+
     } else if (state.temp >= 50) {
         tempValue.classList.add("green");
+        landscape.innerHTML = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+
     } else if (state.temp <= 49) {
         tempValue.classList.add("light-blue");
+        landscape.innerHTML = "❄️🥶🥶❄️🧣🧤❄️🎿🏔️❄️🧥🏂";
     }
 }
 
