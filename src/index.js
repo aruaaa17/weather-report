3874535195dfad66f458a90ae57b6860c8c3ccb2
April Zhang <aprilzh23@gmail.com>
2023-06-07 16:24:27 -0700

    Add event listener for changing color of temp

--- src/index.js
+++ src/index.js
@@ -1,23 +1,53 @@
-// Select increment and decrement buttons
-const incrementCount = document.getElementById("increaseTempControl");
-const decrementCount = document.getElementById("decreaseTempControl");
-
-const changeElement = document.querySelector("#tempValue");
+const state = {
+    temp: 72
+};
 
 // Default value 72;
-let count = 72;
-changeElement.innerHTML = count;
+// let temp = 72;
+// tempValue.innerHTML = temp;
 
 const increaseTemp = () => {
-    count++;
-    changeElement.innerHTML = count;
+    state.temp++;
+    tempValue.innerHTML = state.temp;
+    changeTempColor();
 }
 
 const decreaseTemp = () => {
-    count--;
-    changeElement.innerHTML = count;
+    state.temp--;
+    tempValue.innerHTML = state.temp;
+    changeTempColor();
 }
 
-// Add click event to buttons
-incrementCount.addEventListener("click", increaseTemp);
-decrementCount.addEventListener("click", decreaseTemp);
+
+// Change the color of the temperature text
+const tempValue = document.querySelector("#tempValue");
+
+const changeTempColor = () => {
+    tempValue.classList.remove("red", "orange", "yellow", "green", "light-blue");
+    
+    if (state.temp >= 80) {
+        tempValue.classList.add("red");
+    } else if (state.temp >= 70) {
+        tempValue.classList.add("orange");
+    } else if (state.temp >= 60) {
+        tempValue.classList.add("yellow");
+    } else if (state.temp >= 50) {
+        tempValue.classList.add("green");
+    } else if (state.temp <= 49) {
+        tempValue.classList.add("light-blue");
+    }
+}
+
+const registerEventHandlers = () => {
+    // Select increment and decrement buttons
+    const incrementTemp = document.getElementById("increaseTempControl");
+    const decrementTemp = document.getElementById("decreaseTempControl");
+    const tempValue = document.querySelector("#tempValue");
+
+    // Add click event to buttons
+    incrementTemp.addEventListener("click", increaseTemp);
+    decrementTemp.addEventListener("click", decreaseTemp);
+    tempValue.innerHTML = state.temp;
+}
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
